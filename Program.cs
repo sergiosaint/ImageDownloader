122eeac3a92bb5f4b40a9df50aaf93b434e077ee
sergiosaint <sergio.n.lopes@gmail.com>
2023-05-20 13:18:24 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace ImageDownloader
+{
+    class Program
+    {
+        static string[] urls = {
+            "https://www.pediportugal.com/laura-e-andre-fotos-em-casa-dos-noivos",
+            "https://www.pediportugal.com/laura-e",
+            "https://www.pediportugal.com/laura-e-andre-noivos",
+            "https://www.pediportugal.com/laura-e-andre-sem-noivos" };
+
+        static void Main(string[] args)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                foreach (var url in urls)
+                {
+                    using (HttpResponseMessage response = client.GetAsync(url).Result)
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            string pageCode = content.ReadAsStringAsync().Result;
+
+                            foreach (Match m in Regex.Matches(pageCode, "<img.+?src=[\"'](.+?)[\"'].+?>", RegexOptions.IgnoreCase | RegexOptions.Multiline))
+                            {
+                                string src = m.Groups[1].Value;
+
+                                try
+                                {
+                                    var dotIndex = src.IndexOf(".jpg");
+                                    if (dotIndex == -1) {
+                                        Console.WriteLine("No Image: " + src);
+                                        continue;
+                                    }
+                                    var imgUrl = src.Substring(0, dotIndex) + ".jpg";
+
+                                    using (WebClient webClient = new WebClient())
+                                    {
+                                        var filenName = imgUrl.Substring(35);
+                                        webClient.DownloadFile(imgUrl, filenName);
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
